22b35c76768d21869a711fa3e912b3fdbddd48df
John Brayton <jbrayton@goldenhillsoftware.com>
2016-09-23 21:08:12 -0400

    Framework for use with Carthage

---
+++ Punycode-Cocoa/Punycode-Cocoa.h
@@ -0,0 +1,19 @@
+//
+//  Punycode-Cocoa.h
+//  Punycode-Cocoa
+//
+//  Created by John Brayton on 9/23/16.
+//  Copyright © 2016 John Brayton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Punycode-Cocoa.
+FOUNDATION_EXPORT double Punycode_CocoaVersionNumber;
+
+//! Project version string for Punycode-Cocoa.
+FOUNDATION_EXPORT const unsigned char Punycode_CocoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Punycode_Cocoa/PublicHeader.h>
+
+#import "NSStringPunycodeAdditions.h"
